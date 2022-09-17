0d6610e8b3672cda92aa22a4d5b93a119fab94e2
Fael123Programming <rafaelfonseca1020@gmail.com>
2022-09-16 21:24:13 -0300

    new file

--- cpp-general/oop/ex03/date.cpp
+++ cpp-general/oop/ex03/date.cpp
@@ -1,21 +1,37 @@
 #include <iostream>
-#include <string>
-
-using std::string;
-using std::to_string;
+#include <ctime>
 
 class Date {
     public:
+        Date(); //Default constructor.
+        Date(int, int, int);
         int getDay();
         int getMonth();
         int getYear();
         void setDay(int);
         void setMonth(int);
         void setYear(int);
-        string toString();
+        int getSystemDay();
+        int getSystemMonth();
+        int getSystemYear();
+        std::string toString();
     private:
-        int day, month, year;
+        int day, month, year; //Default value is 0.
 };
+
+Date::Date() : day(getSystemDay()), month(getSystemMonth()), year(getSystemYear()) {}
+
+Date::Date(int d, int m, int y) : day(d), month(m), year(y) {
+    if (day < 1 || day > 31) {
+        day = getSystemDay();
+    }
+    if (month < 1 || month > 12) {
+        month = getSystemMonth();
+    }
+    if (year < 1) {
+        year = getSystemYear();
+    }
+}
 
 int Date::getDay() {
     return day;
@@ -50,6 +66,24 @@ void Date::setYear(int newYear) {
     year = newYear;
 }
 
-string Date::toString() {
-    return to_string(day) + "/" + to_string(month) + "/" + to_string(year);
+int Date::getSystemDay() {
+    time_t t = time(NULL);
+    tm* tt = localtime(&t);
+    return tt->tm_mday;
+}
+
+int Date::getSystemMonth() {
+    time_t t = time(NULL);
+    tm* tt = localtime(&t);
+    return tt->tm_mon + 1;
+}
+
+int Date::getSystemYear() {
+    time_t t = time(NULL);
+    tm* tt = localtime(&t);
+    return tt->tm_year + 1900;
+}
+
+std::string Date::toString() {
+    return std::to_string(day) + "/" + std::to_string(month) + "/" + std::to_string(year);
 }
--- cpp-general/oop/ex03/main.cpp
+++ cpp-general/oop/ex03/main.cpp
@@ -1,20 +1,24 @@
 #include <iostream>
 #include "person.cpp"
 
-using std::cout;
-using std::endl;
-
 int main() {
-    
-    Person p1;
-    Date myDateOfBirth;
-    p1.setName("Rafael Fonseca");
-    myDateOfBirth.setDay(11);
-    myDateOfBirth.setMonth(6);
-    myDateOfBirth.setYear(2002);
-    p1.setDateOfBirth(myDateOfBirth);
-    string result = p1.toString();
-    cout << result << endl;
-
+    // Person p1;
+    // Date myDateOfBirth;
+    // p1.setName("Rafael Fonseca");
+    // myDateOfBirth.setDay(11);
+    // myDateOfBirth.setMonth(6);
+    // myDateOfBirth.setYear(2002);
+    // p1.setDateOfBirth(myDateOfBirth);
+    // string result = p1.toString();
+    // std::cout << result << std::endl;
+    Date d1 = Date(20, 12, 0);
+    std::cout << d1.toString() << std::endl; 
+    Date d2; //Calls default constructor.
+    std::cout << d2.toString() << std::endl;
+    //Date d3(); //WRONG!
+    Date d4(16, 9, 2022);
+    std::cout << d4.toString() << std::endl;
+    //Cast a char to a digit.
+    std::cout << static_cast<int>('6') - static_cast<int>('0') << std::endl;
     return 0;
 }
